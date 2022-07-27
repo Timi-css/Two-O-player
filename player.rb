6b6e8e20aae08cd1dc38c115e94103591ea3fb7a
Timi Alabi <alabitimi65@gmail.com>
2022-07-27 21:16:18 +0000

    First comit - psudo code for planning

---
+++ player.rb
@@ -0,0 +1,26 @@
+# Game starts - intialize players
+# Set player1 as current player
+# Random question generated from question class 
+# Ask current player the question
+# Get and evaluate response from player 
+# If question is wrong reduce player life 
+# Check player life
+# If player life is remaining switch to next player else if player life === 0 end game and declare other player as winner
+
+class Question
+# writes random math questions for the players
+def radomMath
+  def addition(a, b)
+    
+  end
+
+end
+
+class Player
+  #responds to the math question given by class question
+
+end
+
+class Game
+end
+
